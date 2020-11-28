ff1f0295025eb1eed593574b67064b98b572d9a5
Rob Thornton <rthornton128@gmail.com>
2020-11-28 14:59:06 -0500

    Move views into root views directory

--- routes/graphiql.rb
+++ routes/graphiql.rb
@@ -6,7 +6,7 @@ class Application
   hash_routes.on('graphiql') do |r|
     r.get(true) do
       params = r.params
-      path = File.join(APP_ROOT, 'routes', 'views', 'graphiql.erb')
+      path = File.join(APP_ROOT, 'views', 'graphiql.erb')
 
       helper = Helpers::Graphiql.new(params, template_path: path)
       helper.result
