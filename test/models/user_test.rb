5e4ab1e029c3e7e01da229c64934084405b3bef3
Rob Thornton <rthornton128@gmail.com>
2020-11-27 19:25:35 -0500

    Test User a little more thoroughly

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,25 @@ require 'test_helper'
 require 'user'
 
 class TestUser < ApplicationTest
+  def setup
+    User.truncate
+  end
+
   def test_user
-    ::User.new
+    user = ::User.create(first_name: 'User', last_name: 'Name', email: 'user.name@something.com')
+
+    user = User.find(id: user.id)
+
+    assert_equal('User', user.first_name)
+    assert_equal('Name', user.last_name)
+    assert_equal('user.name@something.com', user.email)
+  end
+
+  def test_user_unique
+    ::User.create(first_name: 'User', last_name: 'Name', email: 'user.name@something.com')
+
+    assert_raise(Sequel::UniqueConstraintViolation) do
+      ::User.create(first_name: 'User', last_name: 'Name', email: 'user.name@something.com')
+    end
   end
 end
