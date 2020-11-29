ec3f820f8e364e3590d88082f1c16acb7dfe063f
Rob Thornton <rthornton128@gmail.com>
2020-11-28 21:57:52 -0500

    Add style command for rubocop

--- Rakefile
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-task(default: :test)
-
-task(:test) do
-  puts 'No tests'
-  exit(-1)
-end
--- cli.rb
+++ cli.rb
@@ -20,6 +20,20 @@ class CLI < Thor
     Sequel::Migrator.run(DB, 'migrate')
   end
 
+  desc 'server', 'Run server'
+  def server
+    run('rackup')
+  end
+
+  desc 'style', 'Run rubocop to check style'
+  method_option :apply, type: :boolean, aliases: '-a', desc: 'Safe, non-destructive automatic cop fixes'
+  method_option :unsafe_apply, type: :boolean, aliases: '-A', desc: 'Unsafe, destructive automatic cop fixes'
+  def style
+    run('bundle exec rubocop')
+    run('bundle exec rubocop -a') if options.apply?
+    run('bundle exec rubocop -A') if options.unsafe_apply?
+  end
+
   desc 'test', 'Run tests'
   def test(*_args)
     run('ruby -I. test/all.rb')
