d8a2022c8df0d2ffc7e046c8bb6ab627e719b3cf
Rob Thornton <rthornton128@gmail.com>
2020-11-29 16:31:58 -0500

    Bootsnap (#12)
    * Depend on bootsnap
    
    * Configure bootsnap
    
    Ignore the `routes/` path to prevent zeitwerk loader errors with
    bootsnap

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'bootsnap', require: false
 gem 'bundler'
 gem 'graphql'
 gem 'puma'
--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'bootsnap'
 require 'zeitwerk'
+
+Bootsnap.setup(
+  cache_dir: 'tmp/cache',
+  development_mode: development?,
+  load_path_cache: true,
+  autoload_paths_cache: false,
+  disable_trace: false,
+  compile_cache_iseq: !test?,
+  compile_cache_yaml: true
+)
 
 loader = Zeitwerk::Loader.new
 loader.push_dir(File.join(APP_ROOT, 'graphql'))
@@ -9,5 +20,11 @@ loader.push_dir(File.join(APP_ROOT, 'helpers'))
 loader.push_dir(File.join(APP_ROOT, 'models'))
 loader.push_dir(File.join(APP_ROOT, 'routes'))
 loader.push_dir(File.join(APP_ROOT, 'test')) if test?
+
+# Zeitwerk expects a file to declare a constant matching its filename but
+# routes in this app extend the core Application. Ignore them to prevent
+# errors when preloading with Bootsnap
+loader.ignore(File.join(APP_ROOT, 'routes'))
+
 loader.enable_reloading
 loader.setup
