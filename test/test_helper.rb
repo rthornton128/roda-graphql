9eae5f3c36d0cc88a7199fb39ad2cb76d81048ee
Rob Thornton <rthornton128@gmail.com>
2020-11-30 22:25:43 -0500

    Test fixtures (#16)
    * Add fixture_dependencies gem
    
    * Use fixtures in tests
    
    * Add users fixtures
    
    * Add fixtures support to test_helper
    
    * Use fixture in users test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 require 'test/unit'
+require 'fixture_dependencies/test_unit/sequel'
 require 'mocha/test_unit'
 
+FixtureDependencies.fixture_path = File.join(APP_ROOT, 'test', 'fixtures')
+
 module TestHelper
-  class ApplicationTest < Test::Unit::TestCase; end
+  class ApplicationTest < FixtureDependencies::SequelTestCase
+    include FixtureDependencies::HelperMethods
+  end
 end
