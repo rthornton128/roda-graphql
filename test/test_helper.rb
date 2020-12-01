b96add33a7586a7926c7e63b07855314c42a50e8
Rob Thornton <rthornton128@gmail.com>
2020-11-30 21:56:29 -0500

    Use fixtures in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require 'test/unit'
+require 'fixture_dependencies/test_unit/sequel'
 require 'mocha/test_unit'
 
 module TestHelper
-  class ApplicationTest < Test::Unit::TestCase; end
+  class ApplicationTest < FixtureDependencies::SequelTestCase; end
 end
